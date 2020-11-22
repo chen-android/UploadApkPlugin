4925b2a8f419f27ba57a261deee6b31c10e001ab
cs-mac <1014988429@qq.com>
2020-11-22 09:13:18 +0800

    init

---
+++ src/main/java/com/cs/plugin/UploadApkPlugin.java
@@ -0,0 +1,82 @@
+package com.cs.plugin;
+
+import okhttp3.*;
+import org.apache.commons.lang3.StringUtils;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class UploadApkPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        UploadApkPluginExtension uploadApkPlugin = project.getExtensions().create("UploadApkPgyerParams", UploadApkPluginExtension.class);
+
+        project.task("uploadToPgyer", task -> {
+            task.doLast( lastTask -> {
+                if (StringUtils.isBlank(uploadApkPlugin.apkFileLocation)) {
+                    throw new IllegalArgumentException("apk文件地址没有配置");
+                }
+                if (StringUtils.isBlank(uploadApkPlugin.uKey)) {
+                    throw new IllegalArgumentException("uKey没有配置");
+                }
+                if (StringUtils.isBlank(uploadApkPlugin.apiKey)) {
+                    throw new IllegalArgumentException("apiKey没有配置");
+                }
+                File apkFilePath = new File(uploadApkPlugin.apkFileLocation);
+                if (apkFilePath.exists()) {
+                    if (apkFilePath.isDirectory()) {
+                        //如果配置的是文件夹，则找目录下第一个.apk文件
+                        System.out.println("当前地址：" + uploadApkPlugin.apkFileLocation + "    配置的是目录");
+                        File[] files = apkFilePath.listFiles((dir, name) -> name.endsWith(".apk"));
+                        if (files != null) {
+                            File selectApk = files[0];
+                            System.out.println("选择到apk文件：" + selectApk.getName());
+                            uploadApk(selectApk,uploadApkPlugin.uKey,uploadApkPlugin.apiKey);
+                        }else{
+                            System.out.println("当前目录下没有apk文件");
+                        }
+                    } else {
+                        //配置的直接对应是apk文件
+                        System.out.println("当前地址：" + uploadApkPlugin.apkFileLocation + "    配置的是文件");
+                        System.out.println("选择到apk文件：" + apkFilePath.getName());
+                        uploadApk(apkFilePath,uploadApkPlugin.uKey,uploadApkPlugin.apiKey);
+                    }
+                }else{
+                    throw new RuntimeException("当前地址： " + uploadApkPlugin.apkFileLocation + "不存在");
+                }
+            });
+        });
+    }
+
+    private void uploadApk(File file,String uKey,String apiKey) {
+        String url = "https://www.pgyer.com/apiv1/app/upload";
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        RequestBody fileBody = RequestBody.create(file,MediaType.parse("application/octet-stream"));
+
+        MultipartBody body = new MultipartBody.Builder()
+                .setType(MediaType.parse("multipart/form-data"))
+                .addFormDataPart("file","app.apk",fileBody)
+                .addFormDataPart("uKey", uKey)
+                .addFormDataPart("_api_key",apiKey)
+                .build();
+
+        final Request request = new Request.Builder()
+                .post(body)
+                .url(url)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        try {
+            Response execute = call.execute();
+            boolean successful = execute.isSuccessful();
+            System.out.println("请求结果："+successful);
+            System.out.println("请求结果"+execute.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/cs/plugin/UploadApkPluginExtension.java
@@ -0,0 +1,16 @@
+package com.cs.plugin;
+
+public class UploadApkPluginExtension {
+    public String apkFileLocation;
+    public String uKey;
+    public String apiKey;
+
+    @Override
+    public String toString() {
+        return "com.cs.plugin.UploadApkPluginExtension{" +
+                "apkFileLocation='" + apkFileLocation + '\'' +
+                ", uKey='" + uKey + '\'' +
+                ", apiKey='" + apiKey + '\'' +
+                '}';
+    }
+}
